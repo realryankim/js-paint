a8aa0342f5b197bc49b7b783b75d78ec4b4dded3
realryankim <ryankimofficial@gmail.com>
2021-05-23 15:11:02 +0900

    add draw functionality

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = document.getElementsByClassName('canvas')[0].offsetWidth;
+canvas.height = document.getElementsByClassName('canvas')[0].offsetHeight;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,20 +13,27 @@ const stopPainting = () => {
   painting = false;
 };
 
+const startPainting = () => {
+  painting = true;
+};
+
 const onMouseMove = e => {
   const x = e.offsetX;
   const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 const onMouseDown = e => {
   painting = true;
 };
 
-const onMouseUp = e => {
-  stopPainting();
-};
-
 canvas && canvas.addEventListener('mousemove', onMouseMove),
   canvas.addEventListener('mousedown', onMouseDown),
-  canvas.addEventListener('mouseup', onMouseUp),
+  canvas.addEventListener('mouseup', stopPainting),
   canvas.addEventListener('mouseleave', stopPainting);
